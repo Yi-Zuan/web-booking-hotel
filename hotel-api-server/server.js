de9da59e345433f2ff6884e0f72b7d8814f50b13
Yi-Zuan <leduocha2006@gmail.com>
2025-11-29 14:03:39 +0700

    Fix duong dan file HTML

--- hotel-api-server/server.js
+++ hotel-api-server/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__hotel-api-server));
 
 // --- KẾT NỐI DATABASE BẰNG BIẾN MÔI TRƯỜNG ---
 const dbConnection = mysql.createConnection({
