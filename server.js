dfa8b9e91f2906231ea048fb3791c407aeb9a06e
Yi-Zuan <leduocha2006@gmail.com>
2025-11-29 14:56:06 +0700

    web-booking-hotel

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const dbConnection = mysql.createConnection({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     ssl: { rejectUnauthorized: false }
 });
