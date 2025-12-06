bc35980723f73a7e6d25d73567d01b5ae1cd270a
Yi-Zuan <leduocha2006@gmail.com>
2025-12-06 20:22:02 +0700

    New

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const dbConnection = mysql.createConnection({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
-    password: process.env.PASS,
+    password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     ssl: { rejectUnauthorized: false }
 });
